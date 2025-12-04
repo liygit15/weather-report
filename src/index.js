75c3848bcfe4cc28fc36af800f99a87374e11c0a
Bianca <myname45@gmail.com>
2025-12-04 09:08:11 -0500

    wave 2

--- src/index.js
+++ src/index.js
@@ -4,22 +4,60 @@ const state = {
   cityName: 'Seattle',
   cityInput: null,
   cityDisplay: null,
+  //have to update this to get current temp from API?
+  currentTemp: 60,
+  tempDisplay: null,
+  increaseBtn: null,
+  decreaseBtn: null,
 };
 
 
+// wave 2
 
+//determine color
+const getColorForTemp = (temp) => {
+  if (temp >=80) return 'red';
+  if (temp >= 70) return 'orange';
+  if (temp >= 60) return 'yellow';
+  if (temp >= 50) return 'green';
+  return 'teal';
+};
 
+//determine landscape
+const getLandscapeForTemp = (temp) => {
+  if (temp >= 80) return "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+  if (temp >= 70) return "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+  if (temp >= 60) return "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+  return "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+};
 
+//update temp display
+const updateTemperatureDisplay = () => {
+  state.tempDisplay.textContent = state.currentTemp + '°F';
 
+  const color = getColorForTemp(state.currentTemp);
 
+  state.tempDisplay.style.backgroundColor = color;
+  if (color === 'yellow') {
+    state.tempDisplay.style.color = 'black';
+  } else {
+    state.tempDisplay.style.color = 'white';
+  }
 
+  const landscape = getLandscapeForTemp(state.currentTemp);
+  state.landscapeDiv.textContent = landscape;
+};
 
+//event handling
+const handleIncreaseTemp = () => {
+  state.currentTemp++;
+  updateTemperatureDisplay();
+};
 
-
-
-
-
-
+const handleDecreaseTemp = () => {
+  state.currentTemp--;
+  updateTemperatureDisplay();
+};
 
 
 
@@ -31,16 +69,23 @@ const handleCityInput = (event) => {
 
 const registerEvents = () => {
   state.cityInput.addEventListener('input', handleCityInput);
+  state.increaseBtn.addEventListener('click', handleIncreaseTemp);
+  state.decreaseBtn.addEventListener('click', handleDecreaseTemp);
 };
 
 const loadControls = () => {
   state.cityInput = document.getElementById('cityNameInput');
   state.cityDisplay = document.getElementById('headerCityName');
+  state.tempDisplay = document.getElementById('tempValue');
+  state.increaseBtn = document.getElementById('increaseTempControl');
+  state.decreaseBtn = document.getElementById('decreaseTempControl');
+  state.landscapeDiv = document.getElementById('landscape');
 };
 
 const onLoaded = () => {
   loadControls();
   registerEvents();
+  updateTemperatureDisplay();
 };
 
 onLoaded();
